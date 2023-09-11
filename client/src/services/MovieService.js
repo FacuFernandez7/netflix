f91923671fd869464bfa70ded400d9d98e3be0ea
Facundo Fernandez <72953555+FacuFernandez7@users.noreply.github.com>
2023-09-11 19:11:01 -0300

    restore (#7)

--- client/src/services/MovieService.js
+++ client/src/services/MovieService.js
@@ -1,14 +1,40 @@
 import axios from "axios";
 
+
 const baseURL = "http://localhost:5000/movie/";
 
 export const getMovies = async () => {
   let movies = {};
-  movies = await axios.get(baseURL).then((response) => response.data);
+  movies = await axios.get(baseURL).then((res) => res.data);
   return movies;
 };
 
-export const newMovie = async () => {
-  
+export const newMovie = async ({title,synopsis,score, genre,age}) => {
+  const newMovie = {
+    title,
+    synopsis,
+    score,
+    genre,
+    age
+  }
+  const result = await axios.post(baseURL, newMovie).then(res => res);
+  return result;
+}
+
+export const updateMovie = async (data) => {
+  const updateMovie = {
+    title: data.title,
+    synopsis: data.synopsis,
+    score: data.score,
+    genre: data.genre,
+    age: data.age,
+  }
+  const result = await axios.put(`${baseURL}/${data.id}`, updateMovie).then(res => res.data);
+  return result;
+}
+
+export const deleteMovie = async (id) => {
+  const result = await axios.delete(`${baseURL}/${id}`).then(res => res.status);
+  return result;
 }
 
